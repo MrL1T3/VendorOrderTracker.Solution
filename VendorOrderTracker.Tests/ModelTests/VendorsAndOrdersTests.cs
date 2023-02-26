467577313ce4aba61921ac7b19ec0a8e0c63b2cd
Emil Anton <emilbstn@gmail.com>
2023-02-25 18:20:26 -0800

    add test for description of vendor

--- VendorOrderTracker.Tests/ModelTests/VendorsAndOrdersTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorsAndOrdersTests.cs
@@ -7,11 +7,18 @@ namespace VendorOrderTracker.Tests
   [TestClass]
   public class VendorsAndOrdersTests
   {
+    [Ignore]
     [TestMethod]
-    public void vendorName_EqualsJohn_True()
+    public void vendorName_Equals_John_True()
     {
-      Vendor first = new Vendor("John");
+      Vendor first = new Vendor("John", "");
       Assert.AreEqual("John", first.VendorName);
+    }
+    [TestMethod]
+    public void VendorDesc_Equals_SellsGlassware_True()
+    {
+      Vendor first = new Vendor("John", "John sells glassware products");
+      Assert.AreEqual("John sells glassware products", first.VendorDesc);
     }
   }
 }
