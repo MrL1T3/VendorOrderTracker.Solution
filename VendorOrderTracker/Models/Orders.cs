13bdd1569b950bc72ed74d712de025ba088b1a60
Emil Anton <emilbstn@gmail.com>
2023-03-05 19:56:41 -0800

    add list to Orders.cs to mimic workings of Vendors.cs

--- VendorOrderTracker/Models/Orders.cs
+++ VendorOrderTracker/Models/Orders.cs
@@ -4,6 +4,23 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    public string OrderText{get; set;}
+    public Order()
+    {
+      OrderText = "example order";
+    }
+    public Order(string submittedOrder)
+    {
+      OrderText = submittedOrder;
+    }
+    private static List<Order> sOrders = new List<Order>();
+
+    public static void AddOrder(Order o) {
+      sOrders.Add(o);
+    }
     
+    public static List<Order> OrderList() {
+      return sOrders;
+    }
   }
 }
