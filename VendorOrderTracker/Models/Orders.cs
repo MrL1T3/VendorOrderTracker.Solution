1930184b7ea7899e19b2241c1f90c574f494b77c
Emil Anton <emilbstn@gmail.com>
2023-03-08 12:28:07 -0800

    add OrderDate property

--- VendorOrderTracker/Models/Orders.cs
+++ VendorOrderTracker/Models/Orders.cs
@@ -7,11 +7,13 @@ namespace VendorOrderTracker.Models
     public int VendorID{get; set;}
     public string OrderText{get; set;}
     public string OrderPrice{get; set;}
-    public Order(string submittedOrder, string submittedPrice, int newID)
+    public string OrderDate{get; set;}
+    public Order(string submittedOrder, string submittedPrice, string submittedDate, int newID)
     {
       VendorID = newID;
       OrderText = submittedOrder;
       OrderPrice = submittedPrice;
+      OrderDate = submittedDate;
     }
 
     public static void AddOrder(Order o) {
