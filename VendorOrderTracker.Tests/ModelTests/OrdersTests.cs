012e5e9fc4bfc3b10e20ecaafff84394dd2560cc
Emil Anton <emilbstn@gmail.com>
2023-03-08 12:27:42 -0800

    add test for OrderDate property

--- VendorOrderTracker.Tests/ModelTests/OrdersTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrdersTests.cs
@@ -10,19 +10,19 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderText_Equals_Example_True()
     {
-      Order newOrder = new Order("Example", "", 1);
+      Order newOrder = new Order("Example", "", "", 1);
       Assert.AreEqual("Example", newOrder.OrderText);
     }
     [TestMethod]
     public void VendorID_Equals_One_True()
     {
-      Order newOrder = new Order("Example", "", 1);
+      Order newOrder = new Order("Example", "", "", 1);
       Assert.AreEqual(1, newOrder.VendorID);
     }
     [TestMethod]
     public void AddOrder_Adds_New_Order()
     {
-      Order o = new Order("example", "", 1);
+      Order o = new Order("example", "", "", 1);
       Vendor v = new Vendor("","");
       v.AddNewOrder(o);
       Assert.IsTrue(v.speOrders.Contains(o));
@@ -30,8 +30,14 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderPrice_Equals_Six_Dollars_True()
     {
-      Order newOrder = new Order("Example", "Six Dollars", 1);
+      Order newOrder = new Order("Example", "Six Dollars", "", 1);
       Assert.AreEqual("Six Dollars", newOrder.OrderPrice);
+    }
+    [TestMethod]
+    public void OrderDate_Equals_02_13_10_True()
+    {
+      Order newOrder = new Order("", "", "02/13/10", 1);
+      Assert.AreEqual("02/13/10", newOrder.OrderDate);
     }
   }
 }
