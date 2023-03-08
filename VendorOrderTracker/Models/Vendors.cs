0dd20f19b23cb8f1d49e49a505382390186c08bd
Emil Anton <emilbstn@gmail.com>
2023-03-08 11:30:57 -0800

    make VendorID property able to be set nd changed to iterate more accurately

--- VendorOrderTracker/Models/Vendors.cs
+++ VendorOrderTracker/Models/Vendors.cs
@@ -6,30 +6,29 @@ namespace VendorOrderTracker.Models
   public class Vendor
   {
     private static int sNextID = 0;
-    public int VendorID{get;}
+    public int VendorID{get; set;}
     public string VendorName{get; set;}
     public string VendorDesc{get; set;}
     public List<Order> speOrders = new List<Order>();
     public void AddNewOrder(Order o) {
       speOrders.Add(o);
     }
-    // TODO: need a way to fetch list of orders.
 
     public Vendor()
     {
-      VendorID = ++sNextID;
+      VendorID = 0;
       VendorName = "";
       VendorDesc = "";
     }
     public Vendor(string NewVendor, string NewDesc)
     {
-      VendorID = ++sNextID;
+      VendorID = 0;
       VendorName = NewVendor;
       VendorDesc = NewDesc;
     }
     private static List<Vendor> sVendors = new List<Vendor>();
     public static void AddVendor(Vendor v) {
-      // TODO: assign vendor ID here so doesn't increment more than once.
+      v.VendorID = ++sNextID;
       sVendors.Add(v);
       Console.WriteLine("Added vendor with ID" + v.VendorID);
     }
