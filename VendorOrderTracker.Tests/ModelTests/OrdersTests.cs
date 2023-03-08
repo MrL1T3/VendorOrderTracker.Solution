bd51ad3ac0f4dbe3b78905b62fd1d49d64bf6673
Emil Anton <emilbstn@gmail.com>
2023-03-08 12:32:44 -0800

    update with OrderTitle paremeter assigned

--- VendorOrderTracker.Tests/ModelTests/OrdersTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrdersTests.cs
@@ -10,19 +10,19 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderText_Equals_Example_True()
     {
-      Order newOrder = new Order("Example", "", "", 1);
+      Order newOrder = new Order("", "Example", "", "", 1);
       Assert.AreEqual("Example", newOrder.OrderText);
     }
     [TestMethod]
     public void VendorID_Equals_One_True()
     {
-      Order newOrder = new Order("Example", "", "", 1);
+      Order newOrder = new Order("", "Example", "", "", 1);
       Assert.AreEqual(1, newOrder.VendorID);
     }
     [TestMethod]
     public void AddOrder_Adds_New_Order()
     {
-      Order o = new Order("example", "", "", 1);
+      Order o = new Order("", "", "", "", 1);
       Vendor v = new Vendor("","");
       v.AddNewOrder(o);
       Assert.IsTrue(v.speOrders.Contains(o));
@@ -30,19 +30,19 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderPrice_Equals_Six_Dollars_True()
     {
-      Order newOrder = new Order("Example", "Six Dollars", "", 1);
+      Order newOrder = new Order("", "", "Six Dollars", "", 1);
       Assert.AreEqual("Six Dollars", newOrder.OrderPrice);
     }
     [TestMethod]
     public void OrderDate_Equals_02_13_10_True()
     {
-      Order newOrder = new Order("", "", "02/13/10", 1);
+      Order newOrder = new Order("", "", "", "02/13/10", 1);
       Assert.AreEqual("02/13/10", newOrder.OrderDate);
     }
     [TestMethod]
     public void OrderTitle_Equals_Pastry_Sale_4_True()
     {
-      Order newOrder = new Order("Pastry Sale 4", "", "", "02/13/10", 1);
+      Order newOrder = new Order("Pastry Sale 4", "", "", "", 1);
       Assert.AreEqual("Pastry Sale 4", newOrder.OrderTitle);
     }
   }
