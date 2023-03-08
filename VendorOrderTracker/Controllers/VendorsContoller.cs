aae29846cbada6a3f4e3d13c76b8e2b8ea56323b
Emil Anton <emilbstn@gmail.com>
2023-03-08 11:30:01 -0800

    remove comments and console methods

--- VendorOrderTracker/Controllers/VendorsContoller.cs
+++ VendorOrderTracker/Controllers/VendorsContoller.cs
@@ -6,16 +6,8 @@ namespace VendorOrderTracker.Controllers
 {
   public class VendorsController : Controller
   {
-    // TODO:  [Route("Home/Index/{id?}")]
-    public ActionResult Index(int? id)
+    public ActionResult Index()
     {
-      if(id != null) {
-        Console.WriteLine("You entered id:" + id);
-      } else {
-        Console.WriteLine("no id in route");
-      }
-      
-
       return View(VendorOrderTracker.Models.Vendor.VendorList());
     }
 
@@ -23,7 +15,6 @@ namespace VendorOrderTracker.Controllers
     public ActionResult Vendor(int id) {
       Vendor v = VendorOrderTracker.Models.Vendor.FindVendorByID(id);
       if(v == null) {
-        // TODO: return 404;
         return NotFound();
       }
       return View(v);
@@ -37,7 +28,6 @@ namespace VendorOrderTracker.Controllers
     public ActionResult AddVendorForm(Vendor model)
     {
       VendorOrderTracker.Models.Vendor.AddVendor(model);
-      // TODO: instructions say go back to home page.
       return RedirectToAction("Index", "Home");
     }
   }
